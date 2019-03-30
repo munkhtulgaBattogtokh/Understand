b5e00bfbaa0f67f67be4f12a928738a3b82039f6
Munkhtulga Battogtokh <munkhtulga.battogtokh@kcl.ac.uk>
2019-03-30 09:25:03 +0000

    Render pdf with com.bartekc.pdfviewer

--- app/src/main/java/com/munkhtulga/understand/ReadBookActivity.kt
+++ app/src/main/java/com/munkhtulga/understand/ReadBookActivity.kt
@@ -13,6 +13,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ImageView
 import android.widget.TextView
+import com.github.barteksc.pdfviewer.PDFView
 import java.io.File
 
 
@@ -28,24 +29,32 @@ class ReadBookActivity : AppCompatActivity() {
                     intent.getStringExtra("NO_FILE")
 
         }
-        renderPdfFile()
+        renderPdfFileWithBartekscPdfViewer()
     }
 
-    private fun renderPdfFile() {
-        val file = File(filesDir.absolutePath, "dummy.pdf")
-        val fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
-
-        val bookImageView = findViewById<ImageView>(R.id.bookImageView)
-        val bitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888)
-        val pdfRenderer = PdfRenderer(fileDescriptor)
-        val pageCount = pdfRenderer.pageCount
-
-        val page = pdfRenderer.openPage(0)
-        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-        page.close()
-
-        pdfRenderer.close()
-
-        bookImageView.setImageBitmap(bitmap)
+    // cannot select text
+    private fun renderPdfFileWithBartekscPdfViewer() {
+        //val file = File(filesDir.absolutePath, "sample.pdf")
+        val pdfView = findViewById<PDFView>(R.id.bookPdfView)
+        pdfView.fromAsset("sample.pdf").load()
     }
+
+    // cannot select text
+//    private fun renderPdfFileStandardLibrary() {
+//        val file = File(filesDir.absolutePath, "dummy.pdf")
+//        val fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+//
+//        val bookImageView = findViewById<ImageView>(R.id.bookImageView)
+//        val bitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888)
+//        val pdfRenderer = PdfRenderer(fileDescriptor)
+//        val pageCount = pdfRenderer.pageCount
+//
+//        val page = pdfRenderer.openPage(0)
+//        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+//        page.close()
+//
+//        pdfRenderer.close()
+//
+//        bookImageView.setImageBitmap(bitmap)
+//    }
 }
