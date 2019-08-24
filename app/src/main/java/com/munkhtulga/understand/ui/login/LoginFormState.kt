b7b960f886a99249d819334ffbba888bcc8b51fc
Munkhtulga Battogtokh <munkhtulgabattogtokh@Munkhtulgas-MacBook-Air.local>
2019-08-24 11:02:14 +0800

    Add dummy login activity to the app flow

---
+++ app/src/main/java/com/munkhtulga/understand/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.munkhtulga.understand.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
